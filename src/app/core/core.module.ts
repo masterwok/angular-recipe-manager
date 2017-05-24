64ce3e63bf36f6af6074dee511512e5320390125
Jonathan Trowbridge <jonathan@mapletonhillmedia.com>
2017-05-24 11:52:33 -0600

    Fixed header nav router links.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,12 @@ import {CommonModule} from '@angular/common';
 import {AuthService} from './services/auth.service';
 import {AuthGuard} from './services/auth-guard.service';
 import {ActionButtonsService} from './services/action-buttons.service';
-import {RecipeService} from '../recipe/services/recipe.service';
 import {HeaderComponent} from './header/header.component';
 import {FooterActionButtonsComponent} from './footer-action-buttons/footer-action-buttons.component';
 import {LoginComponent} from './login/login.component';
 import {SharedModule} from '../shared/shared.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
@@ -16,6 +16,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     CommonModule,
     SharedModule,
     FormsModule,
+    RouterModule,
     ReactiveFormsModule,
     FormsModule
   ],
